cddba8e5f2bef0ab472d0d78ad08baaba70ccce0
Lukasz Okraszewski <work@lokraszewski.com>
2021-06-29 19:54:29 +0100

    Updates

---
+++ precache-manifest.04b590e7a6759ba226f0c322e5fad212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca10e3d8200900f16f473ffecaf35c11",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "5b1d813db64c69dde283",
+    "url": "/cv/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "987531bff6c8b16366b1",
+    "url": "/cv/static/js/2.66d19cbb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/cv/static/js/2.66d19cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b1d813db64c69dde283",
+    "url": "/cv/static/js/main.ba42a136.chunk.js"
+  },
+  {
+    "revision": "2036b035f7c64dc17ad7",
+    "url": "/cv/static/js/runtime-main.4afd9069.js"
+  }
+]);
